ca4ac47780e42fd9f3a3c8d29fd7dee220ce5996
Luis <luiscastropess@gmail.com>
2026-04-20 12:47:39 -0300

    Remove todos os arquivos do repositório

--- read-env.js
+++
@@ -1,6 +0,0 @@
-async function readEnv() {
-  const fs = require('fs');
-  const txt = fs.readFileSync('.env', 'utf-8');
-  console.log(txt.split('\n').find(l => l.startsWith('GCP_PRIVATE_KEY')));
-}
-readEnv();
--- test-env2.ts
+++
@@ -1,4 +0,0 @@
-import * as fs from 'fs';
-const envFile = fs.readFileSync('.env', 'utf-8');
-const keyLine = envFile.split('\n').find(l => l.startsWith('GCP_PRIVATE_KEY=')) || "";
-console.log("Extracted Key:", keyLine.substring(keyLine.length - 80));
